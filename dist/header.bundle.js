7f59f05b73ead5000f01cdec5c6168ab27b6c524
Eric JANVIER <janviereric@icloud.com>
2023-08-18 18:14:06 +0200

    Mise en place du JavaScript pour le formulaire

--- dist/header.bundle.js
+++ dist/header.bundle.js
@@ -3,7 +3,6 @@ var __webpack_exports__ = {};
 /*!******************************************!*\
   !*** ./src/assets/javascripts/header.js ***!
   \******************************************/
-console.log("header");
+
 /******/ })()
 ;
-//# sourceMappingURL=header.bundle.js.map
